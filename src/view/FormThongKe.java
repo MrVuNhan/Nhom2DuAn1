7db712cebdac3c0cd44632abbbe8a2c56e4679eb
LAPTOP-0SSDSL4I\Asus <tuoiptph21171@fpt.edu.vn>
2022-12-13 01:21:37 +0700

    sua

--- src/view/FormThongKe.java
+++ src/view/FormThongKe.java
@@ -5,12 +5,27 @@
 package view;
 
 import java.text.DecimalFormat;
+//import java.util.ArrayList;
+//import java.util.List;
+//import javax.swing.JOptionPane;
+//import javax.swing.table.DefaultTableModel;
+import service.impl.ThongKeImpl;
+import viewmodel.ThongKeViewModle;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
-import service.impl.ThongKeImpl;
-import viewmodel.ThongKeViewModle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 /**
  *
@@ -86,6 +101,9 @@ public class FormThongKe extends javax.swing.JFrame {
         txtDoanhThu = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txtSoLuong = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        btnThoat = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -126,6 +144,27 @@ public class FormThongKe extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel2.setText("Tổng Số lượng đã bán :");
 
+        jButton2.setText("Tất cả");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("excel");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        btnThoat.setText("Thoát");
+        btnThoat.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnThoatActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -134,13 +173,21 @@ public class FormThongKe extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 920, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(338, 338, 338))
             .addGroup(layout.createSequentialGroup()
                 .addGap(33, 33, 33)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txtSoLuong, javax.swing.GroupLayout.PREFERRED_SIZE, 383, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txtDoanhThu, javax.swing.GroupLayout.PREFERRED_SIZE, 437, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -157,14 +204,19 @@ public class FormThongKe extends javax.swing.JFrame {
                         .addComponent(jButton1)
                         .addGap(98, 98, 98))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtDoanhThu, javax.swing.GroupLayout.PREFERRED_SIZE, 437, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtSoLuong, javax.swing.GroupLayout.PREFERRED_SIZE, 383, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 0, Short.MAX_VALUE))))
+                        .addComponent(jButton2)
+                        .addGap(38, 38, 38)
+                        .addComponent(jButton3)
+                        .addGap(28, 28, 28))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(338, 338, 338))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnThoat)
+                        .addGap(34, 34, 34))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -181,6 +233,10 @@ public class FormThongKe extends javax.swing.JFrame {
                     .addComponent(txtNam, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
                 .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3))
+                .addGap(8, 8, 8)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 382, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(8, 8, 8)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -190,7 +246,9 @@ public class FormThongKe extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
                     .addComponent(txtDoanhThu, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnThoat)
+                .addContainerGap())
         );
 
         pack();
@@ -205,7 +263,7 @@ public class FormThongKe extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "Mời điền ngày tháng năm muốn tìm kiếm");
         } else if (!ngay.isBlank() && thang.isEmpty() && nam.isEmpty()) {
             if (!ngay.matches("\\d+")) {
-                JOptionPane.showMessageDialog(this, "Ngày là số và là số dương ");
+                JOptionPane.showMessageDialog(this, "Ngày không hợp lệ ");
             } else if (Integer.valueOf(ngay) > 31) {
                 JOptionPane.showMessageDialog(this, "Ngày dưới ngày 31");
             } else {
@@ -214,7 +272,7 @@ public class FormThongKe extends javax.swing.JFrame {
             }
         } else if (!thang.isEmpty() && ngay.isEmpty() && nam.isEmpty()) {
             if (!thang.matches("\\d+")) {
-                JOptionPane.showMessageDialog(this, "Tháng là số và là số dương");
+                JOptionPane.showMessageDialog(this, "Tháng không hơp lệ");
             } else if (Integer.valueOf(thang) > 12) {
                 JOptionPane.showMessageDialog(this, "Tháng chỉ có 12 tháng");
             } else {
@@ -230,11 +288,11 @@ public class FormThongKe extends javax.swing.JFrame {
             }
         } else if (!ngay.isEmpty() && !thang.isEmpty() && nam.isEmpty()) {
             if (!ngay.matches("\\d+")) {
-                JOptionPane.showMessageDialog(this, "Ngày là số");
+                JOptionPane.showMessageDialog(this, "Ngày không hợp lệ");
             } else if (Integer.valueOf(ngay) > 31) {
                 JOptionPane.showMessageDialog(this, "Ngày từ 1 - 31");
             } else if (!thang.matches("\\d+")) {
-                JOptionPane.showMessageDialog(this, "Tháng là số");
+                JOptionPane.showMessageDialog(this, "Tháng không hợp lệ");
             } else if (Integer.valueOf(thang) > 12) {
                 JOptionPane.showMessageDialog(this, "Tháng chỉ có 12 tháng");
             } else {
@@ -247,7 +305,7 @@ public class FormThongKe extends javax.swing.JFrame {
             }
         } else if (ngay.isEmpty() && !thang.isEmpty() && !nam.isEmpty()) {
             if (!thang.matches("\\d+")) {
-                JOptionPane.showMessageDialog(this, "Tháng là số và là số dương");
+                JOptionPane.showMessageDialog(this, "Tháng không hợp lệ");
             } else if (Integer.valueOf(thang) > 12) {
                 JOptionPane.showMessageDialog(this, "Tháng chỉ có 12 tháng");
             } else if (!nam.matches("\\d+") || nam.length() != 4) {
@@ -258,7 +316,7 @@ public class FormThongKe extends javax.swing.JFrame {
             }
         } else if (!ngay.isEmpty() && thang.isEmpty() && !nam.isEmpty()) {
             if (!ngay.matches("\\d+")) {
-                JOptionPane.showMessageDialog(this, "Ngày là số và là số dương");
+                JOptionPane.showMessageDialog(this, "Ngày không hợp lệ");
             } else if (Integer.valueOf(ngay) > 31) {
                 JOptionPane.showMessageDialog(this, "Ngày dưới ngày 31");
             } else if (!nam.matches("\\d+") || nam.length() != 4) {
@@ -272,11 +330,11 @@ public class FormThongKe extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(this, "Năm " + nam + " không là năm nhuận nên không có ngày 29");
             } else {
                 if (!ngay.matches("\\d+")) {
-                    JOptionPane.showMessageDialog(this, "Ngày là số");
+                    JOptionPane.showMessageDialog(this, "Ngày không hợp lệ");
                 } else if (Integer.valueOf(ngay) > 31) {
                     JOptionPane.showMessageDialog(this, "Ngày từ 1 - 31");
                 } else if (!thang.matches("\\d+")) {
-                    JOptionPane.showMessageDialog(this, "Tháng là số");
+                    JOptionPane.showMessageDialog(this, "Tháng không hợp lệ");
                 } else if (Integer.valueOf(thang) > 12) {
                     JOptionPane.showMessageDialog(this, "Tháng chỉ có 12 tháng");
                 } else if (!nam.matches("\\d+") || nam.length() != 4) {
@@ -289,6 +347,119 @@ public class FormThongKe extends javax.swing.JFrame {
             }
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void btnThoatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnThoatActionPerformed
+        Menu fnv = new Menu();
+        fnv.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_btnThoatActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        listCl = thongKe.getAll();
+        showDaTaHD(listCl);
+        DecimalFormat ds = new DecimalFormat("###,###,###,###");
+        thanhTien(listCl);
+        txtDoanhThu.setText(ds.format(thanhTien(listCl)) + " " + "VNĐ");
+        txtSoLuong.setText(ds.format(soLuong(listCl)));
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+       try {
+            XSSFWorkbook xssw = new XSSFWorkbook();
+            XSSFSheet xsss = xssw.createSheet("dsD");
+            XSSFRow xssr = null;
+            Cell cell = null;
+
+            xssr = xsss.createRow(2);
+            cell = xssr.createCell(0, CellType.STRING);
+            cell.setCellValue("listThongKe");
+
+            xssr = xsss.createRow(3);
+            cell = xssr.createCell(0, CellType.STRING);
+            cell.setCellValue("STT");
+
+            cell = xssr.createCell(1, CellType.STRING);
+            cell.setCellValue("Mã NV");
+
+            cell = xssr.createCell(2, CellType.STRING);
+            cell.setCellValue("Tên NV");
+
+            cell = xssr.createCell(3, CellType.STRING);
+            cell.setCellValue("Mã SP");
+
+            cell = xssr.createCell(4, CellType.STRING);
+            cell.setCellValue("Tên SP");
+
+            cell = xssr.createCell(5, CellType.STRING);
+            cell.setCellValue("Ngày Tạo");
+
+            cell = xssr.createCell(6, CellType.NUMERIC);
+            cell.setCellValue("Số lượng bán");
+
+            cell = xssr.createCell(7, CellType.NUMERIC);
+            cell.setCellValue("Giá Nhập");
+
+            cell = xssr.createCell(8, CellType.NUMERIC);
+            cell.setCellValue("Giá Bán");
+            
+            cell = xssr.createCell(9, CellType.NUMERIC);
+            cell.setCellValue("Tiền lời");
+
+            for (int i = 0; i < listCl.size(); i++) {
+                //Modelbook book =arr.get(i);
+                xssr = xsss.createRow(4 + i);
+
+                cell = xssr.createCell(0, CellType.NUMERIC);
+                cell.setCellValue(i + 1);
+
+                cell = xssr.createCell(1, CellType.STRING);
+                cell.setCellValue(listCl.get(i).getIdHD().getIdNV().getMa());
+
+                cell = xssr.createCell(2, CellType.STRING);
+                cell.setCellValue(listCl.get(i).getIdHD().getIdNV().getTen());
+
+                cell = xssr.createCell(4, CellType.STRING);
+                cell.setCellValue(listCl.get(i).getIdCTSP().getIdSp().getMaSP());
+
+                cell = xssr.createCell(5, CellType.STRING);
+                cell.setCellValue(listCl.get(i).getIdCTSP().getIdSp().getTenSP());
+
+                cell = xssr.createCell(3, CellType.STRING);
+                cell.setCellValue(listCl.get(i).getIdHD().getNgayTao());
+
+                cell = xssr.createCell(6, CellType.NUMERIC);
+                cell.setCellValue(listCl.get(i).getSoLuong());
+
+                cell = xssr.createCell(7, CellType.NUMERIC);
+                cell.setCellValue(listCl.get(i).getIdCTSP().getGiaNhap());
+
+                cell = xssr.createCell(8, CellType.NUMERIC);
+                cell.setCellValue(listCl.get(i).getIdCTSP().getGiaBan());
+                
+                cell = xssr.createCell(9, CellType.NUMERIC);
+                cell.setCellValue((listCl.get(i).getIdCTSP().getGiaBan()) - (listCl.get(i).getIdCTSP().getGiaNhap()));
+                
+            }
+
+            File f = new File("D:\\hoadon.xlsx");
+            try {
+                FileOutputStream fis = new FileOutputStream(f);
+                xssw.write(fis);
+                fis.close();
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            JOptionPane.showMessageDialog(this, "in thanh cong");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Loi mo file");
+
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -326,7 +497,10 @@ public class FormThongKe extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnThoat;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
